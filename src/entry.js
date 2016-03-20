51331c55f95162fd0b88baaf8e65ef6d1dfe39f6
Spencer Elliott <me@elliottsj.com>
2016-03-20 11:30:16 -0400

    Basic server_error route

--- src/entry.js
+++ src/entry.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 
 import pify from 'pify';
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import { render } from 'react-dom';
 import { renderToString } from 'react-dom/server';
 import { browserHistory, match, Router, RouterContext } from 'react-router';
@@ -17,11 +17,52 @@ function NoMatch() {
   return <div>404: No matching route</div>;
 }
 
+class ServerError extends Component {
+  static propTypes = {
+    location: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.object,
+    ]),
+  };
+
+  static contextTypes = {
+    router: PropTypes.object,
+  };
+
+  componentDidMount() {
+    const { router } = this.context;
+    const { location } = this.props;
+    if (location.query.destination) {
+      router.push(location.query.destination);
+    }
+  }
+
+  render() {
+    const { location } = this.props;
+
+    return (
+      <div>
+        <h3>Nucleate server error</h3>
+        {location.query.destination ? (
+          <p>Redirected to <em>{location.query.destination}</em>; check the console for errors</p>
+        ) : (
+          <p>No destination specified</p>
+        )}
+      </div>
+    );
+  }
+}
+
+const serverErrorRoute = {
+  path: 'server_error',
+  component: ServerError,
+};
+
 const noMatchRoute = {
   path: '*',
   component: NoMatch,
 };
-const routes = createRoute(siteEntry, '/', [noMatchRoute]);
+const routes = createRoute(siteEntry, '/', [serverErrorRoute, noMatchRoute]);
 
 function renderToDocument(location) {
   // calling `match` is simply for side effects of
@@ -95,7 +136,11 @@ export function renderPath(location) {
             //    with links to assets and an empty <body>
             // 2. After initial render, immediately navigate to `destination`
             // 3. User can then debug their error
-            reject(e);
+
+            renderPath(`/server_error?destination=${location.pathname || location}`)
+              .then(resolve, reject);
+
+            // reject(e);
           }
         });
       } else {
