2218f025ece865cc5b9f9073aa22fd58cd2f2f1a
Spencer Elliott <me@elliottsj.com>
2016-05-08 15:27:03 -0400

    Add type annotations to `awaitValues`

--- src/utils/awaitValues.js
+++ src/utils/awaitValues.js
@@ -1,13 +1,20 @@
-export default function awaitValues(obj) {
-  const promisePairs = Object.keys(obj).map(
-    key => [key, obj[key]]
-  ).map(
+/* @flow */
+
+import fromPairs from 'lodash/fp/fromPairs';
+import toPairs from 'lodash/fp/toPairs';
+
+/**
+ * Given an object with promise values, return a new promise which resolves with an object
+ * containing the resolved values of the given promises, else rejects with an object containing
+ * a single key-value pair containing the reason of the first promise that rejects.
+ */
+export default function awaitValues(
+  obj: { [key: string]: Promise<any> }
+): Promise<{ [key: string]: any }> {
+  const promisePairs = toPairs(obj).map(
     ([key, promise]) => promise.then(value => [key, value])
   );
-  return Promise.all(promisePairs).then(
-    pairs => pairs.reduce(
-      (acc, [key, value]) => ({ ...acc, [key]: value }),
-      {}
-    )
-  );
+  return Promise.all(promisePairs)
+    .then(fromPairs)
+    .catch(([key, reason]) => Promise.reject({ [key]: reason }));
 }
