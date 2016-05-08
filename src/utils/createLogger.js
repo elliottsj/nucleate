73b975ca462c407aa1aa393b0d80579a5fdb87b3
Spencer Elliott <me@elliottsj.com>
2016-05-08 15:27:12 -0400

    Add type annotations to `createLogger`

--- src/utils/createLogger.js
+++ src/utils/createLogger.js
@@ -1,13 +1,27 @@
+/* @flow */
+
 import log from 'npmlog';
+
+type Logger = {
+  error(message: string): void;
+  info(message: string): void;
+  log(message: string): void;
+  silent(message: string): void;
+  silly(message: string): void;
+  verbose(message: string): void;
+  warn(message: string): void;
+};
 
 /**
  * Create a stderr logger with the given prefix.
- * @param  {String} prefix A prefix to all log messages
- * @return {Object}        An object with methods: `silly`, `verbose`, `info`,
- *                         `warn`, `error`, and `silent` which can be used
- *                         to log messages to stderr.
+ * @param  {string} prefix
+ *   A prefix to all log messages
+ * @return {Object}
+ *   An object with methods: `silly`, `verbose`, `info`,
+ *   `warn`, `error`, and `silent` which can be used
+ *   to log messages to stderr.
  */
-export default function createLogger(prefix) {
+export default function createLogger(prefix: string): Logger {
   return {
     log: (level, message, ...args) => {
       log.log(level, /* no prefix */ '', message, ...args);
