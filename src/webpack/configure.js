da358efa320e3c65965e96b0b174d8039590df3a
Spencer Elliott <me@elliottsj.com>
2016-06-04 14:48:07 -0400

    Extract __webpack_chunks__ API into plugin

--- src/webpack/configure.js
+++ src/webpack/configure.js
@@ -2,8 +2,7 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import fs from 'fs';
 import path from 'path';
 import webpack from 'webpack';
-import ConstDependency from 'webpack/lib/dependencies/ConstDependency';
-import BasicEvaluatedExpression from 'webpack/lib/BasicEvaluatedExpression';
+import ChunkApiPlugin from 'webpack-chunks-api-plugin';
 
 const babelLoader = require.resolve('babel-loader');
 const combineLoader = require.resolve('combine-loader');
@@ -92,80 +91,12 @@ export default function configure({
       new ExtractTextPlugin(`${name}.[name].bundle.css`, { allChunks: true }),
       new webpack.optimize.OccurrenceOrderPlugin(),
       ...(commonsChunk ? [
-        new webpack.optimize.CommonsChunkPlugin({ name: 'commons' })
+        new webpack.optimize.CommonsChunkPlugin({ name: 'commons' }),
       ] : []),
       ...(hmr ? [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
       ] : []),
-      {
-        apply(compiler) {
-          compiler.plugin('compilation', (compilation) => {
-            compilation.mainTemplate.plugin('require-ensure', function (source, chunk, hash) {
-              const filename = this.outputOptions.filename || 'bundle.js';
-              const chunkFilename = this.outputOptions.chunkFilename || `[id].${filename}`;
-              const chunkMaps = chunk.getChunkMaps();
-              const assetPath = this.applyPluginsWaterfall("asset-path", JSON.stringify(chunkFilename), {
-                hash: "\" + " + this.renderCurrentHashCode(hash) + " + \"",
-                hashWithLength: function(length) {
-                  return "\" + " + this.renderCurrentHashCode(hash, length) + " + \"";
-                }.bind(this),
-                chunk: {
-                  id: "\" + chunkId + \"",
-                  hash: "\" + " + JSON.stringify(chunkMaps.hash) + "[chunkId] + \"",
-                  hashWithLength: function(length) {
-                    var shortChunkHashMap = {};
-                    Object.keys(chunkMaps.hash).forEach(function(chunkId) {
-                      if(typeof chunkMaps.hash[chunkId] === "string")
-                        shortChunkHashMap[chunkId] = chunkMaps.hash[chunkId].substr(0, length);
-                    });
-                    return "\" + " + JSON.stringify(shortChunkHashMap) + "[chunkId] + \"";
-                  },
-                  name: "\" + (" + JSON.stringify(chunkMaps.name) + "[chunkId]||chunkId) + \""
-                }
-              });
-
-              // surround the chunk-loading code with `'start';` and `'end';` string literals
-              return this.asString([
-                'var isInstalled = !!installedChunks[chunkId];',
-                '(function loadChunk(callback) {',
-                this.indent(source),
-                '}(function() {',
-                this.indent([
-                  'if (!isInstalled) {',
-                  this.indent([
-                    `${this.requireFn}.c.push({`,
-                    this.indent([
-                      'id: chunkId,',
-                      `path: ${this.requireFn}.p + ${assetPath}`,
-                    ]),
-                    '});',
-                  ]),
-                  '}',
-                  'callback.apply(this, arguments);',
-                ]),
-                '}));',
-              ]);
-            });
-            compilation.mainTemplate.plugin('require-extensions', function (source) {
-              return this.asString([
-                source,
-                '',
-                '// __webpack_chunks__',
-                `${this.requireFn}.c = [];`,
-              ]);
-            });
-          });
-          compiler.parser.plugin('expression __webpack_chunks__', function (expr) {
-            const dep = new ConstDependency('__webpack_require__.c', expr.range);
-            dep.loc = expr.loc;
-            this.state.current.addDependency(dep);
-            return true;
-          });
-          compiler.parser.plugin('evaluate typeof __webpack_chunks__', function (expr) {
-            return new BasicEvaluatedExpression().setString('string').setRange(expr.range);
-          });
-        },
-      },
+      new ChunkApiPlugin(),
     ],
   };
 }
